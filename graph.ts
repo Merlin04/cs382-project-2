255fc4fc36b8f1bd331c28d86b0e072184472b19
Patrick Norton <patrick.147.norton@gmail.com>
2023-11-15 16:56:36 -0800

    Add graph implementation

--- graph.ts
+++ graph.ts
@@ -1,21 +1,40 @@
 export class Graph<TKey, TValue> {
+    vertices: Map<TKey, TValue>
+    edges: Map<[TKey, TKey], number>
+
+    constructor() {
+        this.vertices = new Map<TKey, TValue>();
+        this.edges = new Map<[TKey, TKey], number>();
+    }
+
     get_weight(k_u: TKey, k_v: TKey) : number | null {
-        throw "Unimplemented";
+        return this.edges.get([k_u, k_v]) ?? null;
     }
 
     add_vertex(k: TKey, v: TValue, neighbors : TKey[] = []) : void {
-        throw "Unimplemented";
+        this.vertices.set(k, v);
+        // FIXME: Weight of edge
+        for (var neighbor of neighbors) {
+            this.edges.set([k, neighbor], 1);
+            this.edges.set([neighbor, k], 1);
+        }
     }
 
     delete_vertex(k: TKey) : void {
-        throw "Unimplemented";
+        for (var [[k_u, k_v], _] of this.edges) {
+            if (k === k_u || k === k_v) {
+                this.edges.delete([k_u, k_v]);
+            }
+        }
     }
 
-    add_edge(k_u: TKey, k_v: TKey) : void {
-        throw "Unimplemented";
+    add_edge(k_u: TKey, k_v: TKey, w: number) : void {
+        this.edges.set([k_u, k_v], w);
+        this.edges.set([k_v, k_u], w);
     }
 
-    delete_edge(k_u: TKey, k_v: TValue) : void {
-        throw "Unimplemented";
+    delete_edge(k_u: TKey, k_v: TKey) : void {
+        this.edges.delete([k_u, k_v]);
+        this.edges.delete([k_v, k_u]);
     }
 }
